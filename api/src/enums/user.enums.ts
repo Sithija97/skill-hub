fc4713b1ccc3f07f37cef6bb1863e514a263cb6e
Sithija Shehara <nsithijashehara@gmail.com>
2026-06-17 07:10:39 +0530

    cleaned repo

--- api/src/enums/user.enums.ts
+++
@@ -1,6 +0,0 @@
-export enum UserRole {
-  ADMIN = "ADMIN",
-  MANAGER = "MANAGER",
-  VIEWER = "VIEWER",
-  USER = "USER",
-}
--- client/src/models/user.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  id: number;
-  email: string;
-  fullName: string;
-  role: "admin" | "user";
-}
